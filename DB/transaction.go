04765bf4608774168152115b648c48b257589ae8
1421012325 <1421012325@qq.com>
2019-11-05 02:03:35 +0800

    new transaction ??

--- DB/transaction.go
+++ DB/transaction.go
@@ -1,1 +1,20 @@
 package DB
+
+import "gorm"
+
+
+
+// todo Transection 事务封装?
+func Transaction(db *gorm.DB)bool{
+
+	tx := DB.Begin()
+	tx = db
+
+	if tx.Error != nil {
+		tx.Rollback()
+		return false
+	}
+
+	tx.Commit()
+	return true
+}
